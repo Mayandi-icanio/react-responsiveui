f6410ae3ac46871110d73d7a477b588bdf5e7e2a
Mayandi-icanio <mayandi.p@icanio.com>
2023-06-12 09:28:04 +0530

    check push

--- src/Components/header.js
+++ src/Components/header.js
@@ -144,6 +144,7 @@ function ResponsiveAppHeader() {
 
             <Box sx={{ flexGrow: 0 }}>
               <Tooltip title="Open settings">
+                //
                 {/* <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
                   <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
                 </IconButton> */}
